fd675e4856b07e72efb2f6a71f980a08f0db8a15
itcd-cps <itcd-cps@outlook.com>
2024-11-10 14:55:16 +0800

    update group selection dialog component

--- src/app/add-expense/add-expense.component.ts
+++ src/app/add-expense/add-expense.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { GroupSelectionDialog } from '../group-selection-dialog/group-selection-dialog.component'; 
-import { PayerSelectionDialogComponent } from '../payer-selection-dialog/payer-selection-dialog.component'; 
-import { SplitSelectionDialog } from '../split-selection-dialog/split-selection-dialog.component'; 
+import { GroupSelectionDialog } from '../group-selection-dialog/group-selection-dialog.component';
+import { PayerSelectionDialogComponent } from '../payer-selection-dialog/payer-selection-dialog.component';
+import { SplitSelectionDialog } from '../split-selection-dialog/split-selection-dialog.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -13,9 +13,17 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-add-expense',
   standalone: true,
-  imports: [MatDialogModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, CommonModule, FormsModule ],
+  imports: [
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './add-expense.component.html',
-  styleUrl: './add-expense.component.scss'
+  styleUrl: './add-expense.component.scss',
 })
 export class AddExpenseComponent {
   selectedGroup = 'All of Sipidan';
@@ -28,21 +36,24 @@ export class AddExpenseComponent {
 
   openGroupDialog() {
     const dialogRef = this.dialog.open(GroupSelectionDialog);
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) this.selectedGroup = result;
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(result);
+      if (result) {
+        this.selectedGroup = result;
+      }
     });
   }
 
   openPayerDialog() {
     const dialogRef = this.dialog.open(PayerSelectionDialogComponent);
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result) this.selectedPayer = result;
     });
   }
 
   openSplitDialog() {
     const dialogRef = this.dialog.open(SplitSelectionDialog);
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result) this.selectedSplit = result;
     });
   }
--- src/app/group-selection-dialog/group-selection-dialog.component.ts
+++ src/app/group-selection-dialog/group-selection-dialog.component.ts
@@ -1,26 +1,65 @@
-import { Component } from '@angular/core';
-import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import { MatListModule, MatListOption, MatSelectionList } from '@angular/material/list';
+import { Component, Inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-group-selection-dialog',
-  template: `
-    <h2>Select Group</h2>
-    <mat-selection-list #groups>
-      <mat-list-option *ngFor="let group of availableGroups" (click)="selectGroup(group)">
-        {{ group }}
-      </mat-list-option>
-    </mat-selection-list>
-  `,
+  selector: 'app-group-selection-dialog.component',
   standalone: true,
-  imports: [MatDialogModule, MatListModule]
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+  ],
+  templateUrl: './group-selection-dialog.component.html',
+  styleUrls: ['./group-selection-dialog.component.scss'],
 })
 export class GroupSelectionDialog {
-  availableGroups = ['All of Sipidan', 'Group 1', 'Group 2'];
+  myControl = new FormControl('');
+  filteredOptions: string[] = [];
+  selectedGroup: string = '';
 
-  constructor(public dialogRef: MatDialogRef<GroupSelectionDialog>) {}
+  // Hardcoded list of options
+  options: string[] = [
+    'SG',
+    'Shanghai',
+    'Sipidan',
+    'Sze',
+    'Celestine Hoong',
+    'Gabriel Goh',
+  ];
 
-  selectGroup(group: string) {
-    this.dialogRef.close(group);
+  constructor(
+    public dialogRef: MatDialogRef<GroupSelectionDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    // Initialize filtered options with the full list
+    this.filteredOptions = this.options;
+
+    // Update filtered options whenever the input changes
+    this.myControl.valueChanges.subscribe((value) => {
+      this.filterOptions(value || '');
+    });
+  }
+
+  filterOptions(searchTerm: string) {
+    const filterValue = searchTerm.toLowerCase();
+    this.filteredOptions = this.options.filter((option) =>
+      option.toLowerCase().includes(filterValue)
+    );
+  }
+
+  selectOption(option: string) {
+    this.selectedGroup = option;
+    this.closeDialog();
+  }
+
+  closeDialog() {
+    this.dialogRef.close(this.selectedGroup);
   }
 }
