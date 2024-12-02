44e3009510fdabc3611654ee1b84ec7e4f287d76
karooi <karooi88888.com>
2024-12-02 10:01:13 +0800

    add recent group feature

--- src/app/group-selection-dialog/group-selection-dialog.component.ts
+++ src/app/group-selection-dialog/group-selection-dialog.component.ts
@@ -25,8 +25,10 @@ import { Group } from '../models/splitwise.model';
 export class GroupSelectionDialog {
   myControl = new FormControl('');
   filteredOptions: Group[] = [];
-  selectedGroup?: Group;
+  selectedGroup!: Group;
   groups: Group[] = [];
+  recents: Group[] = [];
+  filteredRecents: Group[] = [];
 
   constructor(
     public dialogRef: MatDialogRef<GroupSelectionDialog>,
@@ -47,6 +49,12 @@ export class GroupSelectionDialog {
       this.filteredOptions = this.groups;
     }
 
+    const groupRecents = localStorage.getItem('groupRecents');
+    if (groupRecents) {
+      this.recents = JSON.parse(groupRecents);
+      this.filteredRecents = this.recents;
+    }
+
     this.splitwiseService.getGroups().subscribe((data) => {
       this.groups = data.groups;
       this.filteredOptions = this.groups;
@@ -63,6 +71,9 @@ export class GroupSelectionDialog {
     this.filteredOptions = this.groups.filter((group) =>
       group.name.toLowerCase().includes(filterValue)
     );
+    this.filteredRecents = this.recents.filter((group) =>
+      group.name.toLowerCase().includes(filterValue)
+    );
   }
 
   selectOption(option: Group) {
@@ -71,6 +82,14 @@ export class GroupSelectionDialog {
   }
 
   closeDialog() {
+    if (this.selectedGroup) {
+      this.recents = this.recents.filter(
+        (group) => group !== this.selectedGroup
+      );
+      this.recents.unshift(this.selectedGroup);
+      this.recents = this.recents.slice(0, 3);
+      localStorage.setItem('groupRecents', JSON.stringify(this.recents));
+    }
     this.dialogRef.close(this.selectedGroup);
   }
 }
